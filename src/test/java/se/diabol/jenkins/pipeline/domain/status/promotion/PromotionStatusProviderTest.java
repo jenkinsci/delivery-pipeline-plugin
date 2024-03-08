f453ae8655f0e568a6a4b8d90dddd53eefed8283
Gianluca Soffredini <gianluca.soffredini_ext@lpsolutions.com>
2024-03-08 18:51:04 +0100

    [JENKINS-72527] Fixed compilation erros in PromotionStatusProviderTest.

--- src/test/java/se/diabol/jenkins/pipeline/domain/status/promotion/PromotionStatusProviderTest.java
+++ src/test/java/se/diabol/jenkins/pipeline/domain/status/promotion/PromotionStatusProviderTest.java
@@ -21,8 +21,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.anyObject;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -79,7 +78,7 @@ public class PromotionStatusProviderTest {
 
         final PromotionStatusProvider.PromotedBuildActionWrapper mockPromotedBuildActionWrapper =
                 mock(PromotionStatusProvider.PromotedBuildActionWrapper.class);
-        when(mockPromotedBuildActionWrapper.getPromotions(anyObject())).thenReturn(mockPromotionList);
+        when(mockPromotedBuildActionWrapper.getPromotions(any(Object.class))).thenReturn(mockPromotionList);
 
         final PromotionStatusProvider promotionStatusProvider = new PromotionStatusProvider();
         promotionStatusProvider.setPromotedBuildActionWrapper(mockPromotedBuildActionWrapper);
@@ -128,7 +127,7 @@ public class PromotionStatusProviderTest {
         mockPromotions.add(mockPromotionFirst);
 
         final PromotionStatusProvider.PromotionStatusWrapper mockPromotionStatusWrapper = mock(PromotionStatusProvider.PromotionStatusWrapper.class);
-        when(mockPromotionStatusWrapper.getPromotionBuilds(anyObject())).thenReturn(mockPromotions);
+        when(mockPromotionStatusWrapper.getPromotionBuilds(any(Object.class))).thenReturn(mockPromotions);
 
         final List<Status> mockStatusList = new ArrayList<>();
         PowerMockito.mockStatic(Status.class);
@@ -155,7 +154,7 @@ public class PromotionStatusProviderTest {
 
         final PromotionStatusProvider.PromotedBuildActionWrapper mockPromotedBuildActionWrapper =
                 mock(PromotionStatusProvider.PromotedBuildActionWrapper.class);
-        when(mockPromotedBuildActionWrapper.getPromotions(anyObject())).thenReturn(mockStatusList);
+        when(mockPromotedBuildActionWrapper.getPromotions(any(Object.class))).thenReturn(mockStatusList);
 
         final PromotionStatusProvider promotionStatusProvider = new PromotionStatusProvider();
         promotionStatusProvider.setPromotedBuildActionWrapper(mockPromotedBuildActionWrapper);
