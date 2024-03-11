4ef804222ca524a6d2e5589083f56a0a437bb8e9
Gianluca Soffredini <gianluca.soffredini_ext@lpsolutions.com>
2024-03-11 11:15:26 +0100

    [JENKINS-72527] Fixed all the spotbugs issues.

--- src/main/java/se/diabol/jenkins/workflow/step/TaskStepExecution.java
+++ src/main/java/se/diabol/jenkins/workflow/step/TaskStepExecution.java
@@ -34,6 +34,8 @@ import java.util.logging.Logger;
 
 public class TaskStepExecution extends AbstractStepExecutionImpl {
 
+    private static final long serialVersionUID = 1661891433860818355L;
+
     private static final Logger LOG = Logger.getLogger(TaskStepExecution.class.getName());
 
     @StepContextParameter
@@ -75,6 +77,7 @@ public class TaskStepExecution extends AbstractStepExecutionImpl {
 
     private static final class TaskActionImpl extends InvisibleAction implements TaskAction, Serializable {
 
+        private static final long serialVersionUID = -7007957945134870234L;
         private final transient FlowNode associatedNode;
         private final String taskName;
         private Long finishedTime = null;
